3dd452634456a0b6d32fe1d4fde76a858b15f86e
badcarolwork <85211809+badcarolwork@users.noreply.github.com>
2026-02-11 11:42:36 +0800

    v1

---
+++ script.js
@@ -0,0 +1,149 @@
+const srcClickMarco = "CLICK_URL_UNESC";
+const imgViewMarco = "VIEW_URL_UNESC";
+const ftClickMarco = "CLICK_URL_ESC";
+
+const catfishVideo = `+'<style>body{margin: 0;}div[id^="ftdiv"],iframe[id^="ftframe"]{width:100%!important;height:100%!important;aspect-ratio:16/9;max-height:150px;}</style>';
+var kultCatfishJs = document.createElement("script");
+var kultCatfishCss = document.createElement("style");
+var kultCatfishImp = new Image(1,1);
+kultCatfishImp.src = "%%VIEW_URL_ESC%%";
+kultCatfishCss.innerHTML = \`div[data-ad-type="catfish"],div[id*="Catfish"],div[id*="catfish"]{width: 100% !important;max-width: 400px;height: auto !important;aspect-ratio: 16/9;position: fixed;right: 0;bottom: 0;display: flex}div[id*="Catfish"],div[id*="catfish"]{justify-content: center;z-index: 2147483647;max-height: 225px}div[id*="google_ads"][id*="Catfish"]{width: 100% !important;height: auto !important;aspect-ratio: 16/9;max-width: 400px;max-height: 225px}div[data-ad-type="catfish"] div[id^="div-gpt-ad"],div[data-ad-type="catfish"] div[id^="google_ads_iframe"],div[id^="ftdiv"],div[id^="ftdiv"] iframe{width: 100% !important;height: auto !important;aspect-ratio: 16/9}div.ats-overlay-bottom-wrapper-rendered{z-index: 10 !important}div[data-ad-type="catfish"]{justify-content: center;z-index: 2147483647}@media screen and (max-width: 600px){div[data-ad-type="catfish"],div[id*="Catfish"],div[id*="catfish"],div[id*="google_ads"][id*="Catfish"],div[id^="ftdiv"],div[id^="ftdiv"] iframe{max-width: unset;max-height: 150px}}\`;
+kultCatfishJs.innerHTML = \`const ccobserver=new MutationObserver((()=>{window.addEventListener("message",(function(e){const t=e.data;if("iframe"===t.source&&"requestCollapseFrame"===t.event){const e=['div[class*="catfish-oop"]','div[data-ad-type="catfish"]','div[id*="Catfish_OOP"]','div[id*="catfish_OOP"]'];!function(){for(const t of e){const e=document.querySelectorAll(t);e.length&&e.forEach((e=>e.style.setProperty("display","none","important")))}}()}}))}));ccobserver.observe(document.body,{childList:!0,subtree:!0});\`;
+var parentDoc = window.parent.document;
+    parentDoc.body.appendChild(kultCatfishJs);
+    parentDoc.body.append(kultCatfishCss);
+    parentDoc.body.append(kultCatfishImp);
+document.write(ftTag);
+</script>`;
+const catfishBanner = `+'<style>body{margin: 0;}div[id^="ftdiv"],iframe[id^="ftframe"]{width:100%!important;height:100%!important;aspect-ratio:16/9;max-height:150px;}</style>';
+var kultCatBannerImpTracker = new Image(1,1);
+kultCatBannerImpTracker.src = "%%VIEW_URL_UNESC%%";
+var kultCatBannerjs = document.createElement("script");
+var kultCatBannercss = document.createElement("style");
+  kultCatBannercss.innerHTML = \`div[id*="Catfish"][id*="NISB"],div[id*="Catfish"][id*="ARSB"],div[id*="catfish"][id*="gempak"],div[id*="catfish"][id*="stadium"]{width:100%!important;height:auto!important;aspect-ratio:375/175;position:fixed;right:0;bottom:0;display:flex;justify-content:center;z-index:2147483647;max-width: 450px;max-height: 210px;}
+    div[id*="google_ads"][id*="Catfish"][id*="NISB"],div[id*="google_ads"][id*="catfish"]{width:100%!important;height:auto!important;aspect-ratio:375/175;max-width: 450px;max-height: 210px;}div.ats-overlay-bottom-wrapper-rendered{z-index:10!important;}div[data-ad-type="catfish"]{width:100%!important;height:auto!important;aspect-ratio:375/175;position:fixed;right:0;bottom:0;display:flex;justify-content:center;z-index:2147483647;max-width: 450px;max-height: 210px;}div[data-ad-type="catfish"] div[id^="div-gpt-ad"],div[data-ad-type="catfish"] div[id^="google_ads_iframe"]{width:100%!important;height:auto!important;aspect-ratio:375/175;max-width: 450px;max-height: 210px;}\`;
+  kultCatBannerjs.innerHTML = \`const siteNI = document.querySelector('div[id*="Catfish"][id*="NISB"]');const siteAR = document.querySelector('div[id*="Catfish"][id*="ARSB"]');const divNi = document.querySelector('div[data-ad-type="catfish"] div[id^="div-gpt-ad"]');
+  const divOther = document.querySelector('div[data-ad-type="catfish"]'); const ccobserver=new MutationObserver((()=>{window.addEventListener("message",(function(e){const s=e.data;"iframe"===s.source&&"requestCollapseFrame"===s.event&&(siteNI&&(siteNI.style.display="none"),siteAR&&(siteAR.style.display="none"),divOther&&(divOther.style.display="none"))}))}));ccobserver.observe(document.body,{childList:!0,subtree:!0});\`;
+var parentDoc = window.parent.document;
+    parentDoc.body.appendChild(kultCatBannerjs);
+    parentDoc.body.append(kultCatBannercss);
+    parentDoc.body.append(kultCatBannerImpTracker);
+document.write(ftTag);
+</script>`;
+const inviewVideo =`+'<style>body{margin: 0;}[id^="ftdiv"],[id^="ftdiv"] iframe{width:100%!important;height:100%!important;min-width:444px!important;min-height:250px!important;}</style>';
+var kultOopImp = new Image(1,1);
+kultOopImp.src = "%%VIEW_URL_ESC%%";
+var kultInArticle = document.createElement("style");
+kultInArticle.innerHTML = \`div[id*="OOP_InArticle"],iframe[id*="OOP_InArticle"]{width:100%!important;height:100%!important;min-width:444px!important;min-height:250px!important;margin-bottom: 15px;};\`
+var parentDoc = window.parent.document;parentDoc.body.append(kultInArticle)parentDoc.body.append(kultOopImp);
+document.write(ftTag);
+</script>`;
+
+/// for Skinner customJS
+let skinnerID = "";
+const skinnerJs1 =`+'<style>body{margin: 0;}[id^="ftdiv"],[id^="ftdiv"] iframe{width:100%!important;height:100%!important;}</style>';`;
+let skinnerJs2 = "";
+const skinnerJs3 = 'var kultAdDiv = window.top.document.body.contains(document.getElementById("kultAdDivWrapper"));const KultDivexisting = window.top.document.getElementById("kultAdDivWrapper");if (KultDivexisting) KultDivexisting.remove();var adDiv = window.document.createElement("div"); adDiv.id = "kultAdDivWrapper";window.top.document.body.appendChild(adDiv);';
+const skinnerJs4 = `var adz_doc = '<body style="margin: 0;"><style>body.kultSkin{zoom: 80%;height: auto;}.kultSkin .app:first-child{display:none;}.kultSkin #kultAdDivWrapper{position: fixed; left: 0;top: 0;width: 100%; height: 100%; z-index:1;}.kultSkin #topAdDiv{height: 250px; opacity: 0;}[class^="HeaderAnimation_header_wrapper"]{top: 0px; position: relative;}.kultSkin [class^="HeaderAnimation_header_placeholder"]{display: none !important;}.kultSkin .navbar .container{padding: 0 25px;}.kultSkin #app,.kultSkin .app-container,.kultSkin #__nuxt,.kultSkin #td-outer-wrap{max-width: 1280px;min-width:unset!important;width:100%;overflow: hidden;margin: 0px auto;z-index: 2;position: relative;top: 0px;background: white;}.kultSkin .tdc-row.stretch_row_content,.kultSkin .tdc-row.stretch_row_1200,.kultSkin .tdc-row{width: 100% !important; max-width: 1280px!important; margin: unset !important; padding: unset!important;}.kultSkin .tdc-row[class*="stretch_row"]>.td-pb-row>.td-element-style{width: 100%!important;}.kultSkin .tdc-zone{padding: 0 20px;}.kultSkin .td-header-desktop-wrap .tdc-zone{padding: 0;}.kultSkin .td-header-desktop-sticky-wrap{display: none!important;}.kultSkin .tdi_50:before{width:100%!important;}.kultSkin .wpb_column .wpb_wrapper{text-align: left!important;}.kultSkin .tdb-logo-a{justify-content: start!important;width: fit-content;margin-left: 30px;}.kultSkin .astro-login-button{margin-left: 70px;}.kultSkin .tdb-template #topAdDiv{height:0!important;}.kultSkin .Advertisement_ads_container__4nQpR,.kultSkin .AdvertisementContainer_adsContainer__tKpDh{display:none;}.kultSkin [data-ad-type="mastheadTop"], .kultSkin [class*="masthead"], .kultSkin [id*="Masthead"] {display: none;}.kultSkin #navigation-menu-user-container{top: 70px; right: 17px; position:absolute;}.kultSkin #user-menu{top: 56px!important;right: 0px!important;left: unset!important;position:absolute;}.kultSkin .cZdIOg .sticky{position: absolute!important;top: 0px !important;}.kultSkin .container{padding:0 10px;}.kultSkin #dfp-popup-modal{width:100%;height:100%;}.kultSkin.ENUS{zoom:85%;}.kultSkin.ENUS #form{max-width:1280px;overflow:hidden;margin:0px auto;position:relative;top:0px;background-color: #fff;z-index:2;width:100%;left:8px;}.kultSkin.ENUS .modal-backdrop.show{display:none;}.kultSkin.ENUS header.nav-fixed{left: 0 !important;right: 0 !important;margin: auto;}.kultSkin.ENUS .player .bar{width:1275px;left:0;right:0; margin:auto;}.kultSkin.ENUS #full-ads{display: none;}.kultSkin.ENUS #interstitials{isolation:isolate;z-index:10000;width:100%;height:100%;opacity:1;}.modal-content{box-shadow: unset;}[class^="Header_header_main_container_logos"]{padding:10px;}.app-container_content, .page__header_content, [class^="sc-ifAKCX"]{zoom: 98%}.header-sub{padding: 0 10px;} .hero-carousel-slider{zoom:98%;}.header .fixed-header { position: unset!important;}.kultSkin .dm-story.dm-fullscreen{zoom:127.6%}.kultSkin.xuan .header .fixed-header{position:fixed !important;max-width:1280px;margin:auto;left:0;right:0;}@media (min-width:320px) and (max-width: 767px){.kultSkin.ENUS #form{left:unset}body.kultSkin, .kultSkin.ENUS{zoom:100%;height:auto;}.kultSkin.xuan .hero-carousel-slider.swiper,.kultSkin.xuan .dailymotion-reels-slider,.kultSkin.xuan .carousel-thumb-slider.swiper{width: calc(100vw + 20px);max-width: unset;top: 0;left: 0;right: 0;margin-left: -10px;margin-right: 0;}}</style><iframe id="kultIframe" allow="autoplay; geolocation" allowfullscreen="true" width="100%" height="100%" scrolling="no" marginheight="0" marginheight="0" hspace="0" vspace="0" frameborder="0"></iframe></b';adz_doc += '\ody>';`;
+const skinnerJs5 = `var adz_js = \`var elementExists=document.getElementById("topAdDiv");if(!elementExists){var topAdDiv=window.document.createElement("div");topAdDiv.id="topAdDiv",window.top.document.body.prepend(topAdDiv)}function checkAndToggleSearchClass(){const e=document.getElementById("kultAdDivWrapper");window.location.pathname.includes("/search")?(e&&e.remove(),document.body.classList.remove("kultSkin")):document.body.classList.add("kultSkin")}checkAndToggleSearchClass();const originalPushState=history.pushState,originalReplaceState=history.replaceState;function triggerUrlChange(){checkAndToggleSearchClass(),window.dispatchEvent(new Event("urlchange"))}let checkAdframe;history.pushState=function(...e){originalPushState.apply(this,e),triggerUrlChange()},history.replaceState=function(...e){originalReplaceState.apply(this,e),triggerUrlChange()},window.addEventListener("popstate",checkAndToggleSearchClass);let scrollPoint=!1;checkAdframe=setInterval((function(){if(document.querySelector("iframe[id*='kultIframe']")){const e=document.querySelector("iframe[id*='kultIframe']").contentWindow.document;if(e.querySelector("iframe")){const t=e.querySelector("iframe");console.log("adframe init"),window.addEventListener("scroll",(function(){const e=window.scrollY||window.pageYOffset;e>200&&!scrollPoint?(t.contentWindow.postMessage({source:"kult",event:"kultEvent",data:{text:"kscrollReveal"}},"*"),scrollPoint=!0):e<200&&scrollPoint&&(t.contentWindow.postMessage({source:"kult",event:"kultEvent",data:{text:"kscrollTop"}},"*"),scrollPoint=!1)})),clearInterval(checkAdframe)}}}),500),document.querySelector(".kultSkin .ads-interstitials")&&document.querySelector(".kultSkin .ads-interstitials").classList.add("modal-backdrop");\`;`;
+const skinnerJs6 = `const adz_js_div = window.document.createElement("script");
+adz_js_div.textContent = adz_js;
+adz_js_div.id = "kultSkinJs";
+const kultAdzJsDiv = window.top.document.getElementById("kultSkinJs");
+if (!kultAdzJsDiv){
+    window.top.document.body.appendChild(adz_js_div);
+}
+adDiv.innerHTML = adz_doc;
+// adDiv.innerHTML += adz_js;
+var iframe = window.top.document.getElementById("kultIframe");
+iframe.contentWindow.document.open();
+iframe.contentWindow.document.write(scriptMacro);
+iframe.contentWindow.document.write(ftTag);
+iframe.contentWindow.document.close();
+</script>`;
+
+const resultDiv = document.querySelector("#resultPart");
+const resultText = document.querySelector("#result");
+const skinnerIDText =document.querySelector("#inputSkinnerID");
+const copyBtn = document.getElementById("copy");
+const copyGuide = document.getElementById("copyGuide");
+let isSkinner = false; 
+
+function handleResult(){
+    resultText.focus();
+    resultText.disabled = false;
+    copyBtn.disabled = false;
+    copyGuide.style.display = "block";
+    copyBtn.addEventListener("click", copyResult);
+}
+
+function injectValues(format,tag,skinner) {
+    let result = tag;
+    result = result.replace(
+        /href\s*=\s*([\"\'])(?!\1)/i,
+        `href=$1%%${srcClickMarco}%%`
+    );
+    result = result.replace(
+        /src\s*=\s*([\"\'])(?!\1)/i,
+        `src=$1%%${imgViewMarco}%%`
+    );
+    result = result.replace(
+        /ftClick\s*=\s*["']{1,2}\s*["']?\s*;/gi,
+        `ftClick="%%${ftClickMarco}%%";`
+    );
+    result = result.replace(
+        /ftTag\s*\+=\s*["']>\s*["']?\s*\+\s*ftBuildTag2\s*\+?\s*["']script>["']?\s*;?\s*document\.write\s*\(\s*ftTag\s*\)\s*;?\s*<\/script\s*>/i,
+        `ftTag += '>' + ftBuildTag2 + 'script>'${format === 'catfishvideo' ? catfishVideo : format === 'catfish' ? catfishBanner : format === 'inviewvideo' ? inviewVideo : format === 'skinner' ? skinnerJs1+skinnerJs2+skinnerJs3+skinnerJs4+skinnerJs5+skinnerJs6 : 'no valid format find'}`
+    )
+    handleResult()
+    return resultText.innerHTML = result; 
+}
+
+function validate(format, tag) {
+  if (!format) window.alert("please select a format");
+  if (!tag) window.alert("please insert the creative tag");
+  if(isSkinner && !skinnerID) window.alert("Please fill in the placement ID");
+
+  if (format && tag) {
+    injectValues(format, tag);
+  }
+}
+
+function copyResult() {
+  const textarea = document.getElementById("result");
+  if(!textarea.value || !textarea.value.trim()){
+   window.alert("Kindly procees to setup up the creative tags!");
+  }else{
+    textarea.select();
+    textarea.setSelectionRange(0, textarea.value.length);
+    document.execCommand("copy");
+    window.alert("Copied!");    
+  }  
+}
+
+window.onload = function(){
+    document.getElementById("tagForm").addEventListener("submit", function (e) {
+        e.preventDefault();
+        const format = document.getElementById("formats").value;
+        const tag = document.getElementById("tags").value;
+
+        if(isSkinner){
+           skinnerID = document.getElementById("skinnerID").value; 
+           console.log(skinnerID)
+           skinnerJs2 = 'var scriptMacro = `<`+\`script>var ftClick = "%%CLICK_URL_ESC%%";var ftClick_'+skinnerID+' = ftClick;</`+`script>`;'
+        }
+        validate(format,tag);
+    });
+
+    document.getElementById("formats").addEventListener("change",function(e){
+        const selectedV = e.target.value;
+        if(selectedV === "skinner"){
+            isSkinner = true;
+            skinnerIDText.style.display = "block";
+        }else{
+            isSkinner = false;
+            skinnerIDText.style.display = "none";
+        }
+    })
+   
+}
